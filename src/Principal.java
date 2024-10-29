19da253549a0a68d4ff26b862b39ffd0ff013835
gabriel <gabriel--fiss@hotmail.com>
2024-10-29 20:34:29 -0300

    feat: screematch init

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,76 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
         Filme filme1 = new Filme();
-        filme1.nome = "Carros";
-        filme1.anoDeLancamento = 2001;
-        filme1.duracaoEmMinutos= 120;
-        filme1.incluidoNoPlano = true;
+        filme1.setNome("Carros");
+        filme1.setAnoDeLancamento(2008);
+        filme1.setDuracaoEmMinutos(120);
+        filme1.setIncluidoNoPlano(true);
+        System.out.println("Duracao do filme: " + filme1.getDuracaoEmMinutos());
 
-        filme1.avalia(3.2);
+        filme1.avalia(7.2);
         filme1.avalia(10);
-        filme1.avalia(5);
+        filme1.avalia(8);
         filme1.exibeFichaTecnica();
+        System.out.println(filme1.getTotalDeAvaliacoes());
+        System.out.println(filme1.calculaMediaAvaliacoes());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2010);
+
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(8);
+        lost.setMinutosPorEpisodios(50);
+        lost.avalia(2.0);
+        lost.avalia(2);
+        lost.exibeFichaTecnica();
+        System.out.println("Duracao para maratonar: " + lost.getDuracaoEmMinutos());
+
+
+        Filme filme2 = new Filme();
+        filme2.setNome("Avatar");
+        filme2.setAnoDeLancamento(2001);
+        filme2.setDuracaoEmMinutos(100);
+
+        CalculadoraDeTempo calculadoraDeTempo = new CalculadoraDeTempo();
+        calculadoraDeTempo.inclui(filme2);
+        calculadoraDeTempo.inclui(filme1);
+        calculadoraDeTempo.inclui(lost);
+        System.out.println(calculadoraDeTempo.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(lost);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
+
+        Filme filme3 = new Filme();
+        filme3.setNome("Carros 2 ");
+        filme3.setAnoDeLancamento(2011);
+        filme3.setDuracaoEmMinutos(120);
+        filme3.avalia(8);
+
+        ArrayList<Serie> listaSeries= new ArrayList<>();
+        listaSeries.add(lost);
+        System.out.println(listaSeries.size());
+
+        ArrayList<Filme> listaFilmes = new ArrayList<>();
+        listaFilmes.add(filme1);
+        listaFilmes.add(filme2);
+        listaFilmes.add(filme3);
+        System.out.println("Tamanho da lista: " + listaFilmes.size());
+        System.out.println("Primeiro filme: " + listaFilmes.get(0).getNome());
+        System.out.println(listaFilmes);
     }
 }
---
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,52 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Serie extends Titulo implements Classificavel {
+    private int temporadas ;
+    private boolean ativa;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodios;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodios() {
+        return minutosPorEpisodios;
+    }
+
+    public void setMinutosPorEpisodios(int minutosPorEpisodios) {
+        this.minutosPorEpisodios = minutosPorEpisodios;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return  temporadas  * episodiosPorTemporada * minutosPorEpisodios;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int) calculaMediaAvaliacoes() /2 ;
+    }
+}
